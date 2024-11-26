ee678c7f36ed1c49a038f005d65822e9db323271
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-26 16:05:23 +0100

    ADD: repo/ set-up

---
+++ react-task-list/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { tasks } from './data/tasks.js'
+
+function App() {
+  /*
+  return arr.filter(el => {
+    return el % 2 === 0;
+  }).map(el => (<h1>{el}</h1>))
+  */
+}
+
+export default App
