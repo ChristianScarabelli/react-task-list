035a14f70b97667f55905b28f70d5b8256f8090d
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-26 16:52:11 +0100

    ADD: current-tasks list

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -2,11 +2,50 @@ import './App.css'
 import { tasks } from './data/tasks.js'
 
 function App() {
-  /*
-  return arr.filter(el => {
-    return el % 2 === 0;
-  }).map(el => (<h1>{el}</h1>))
-  */
+  const currentTasks = tasks.filter((task => task.state === 'backlog' || task.state === 'in_progress'))
+  const completedTasks = tasks.filter((task => task.state === 'completed'))
+  // console.log(currentTasks, completedTasks)
+
+  return (
+    <>
+      <footer className='footer'>
+        <div className='container'>
+          <div className="task-manager">
+            <h1>Task Manager</h1>
+          </div>
+        </div>
+      </footer>
+      <main>
+        <section className='current-tasks'>
+          <div className='container'>
+            <h2>Current Tasks ({currentTasks.length})</h2>
+            <div className='current-tasks-list'>
+              <ul>
+                {currentTasks.map(task => (
+                  <li key={task.id}>
+                    <h3>{task.title}</h3>
+                    <p>Priority: {task.priority}</p>
+                    <p>Est. time: {task.estimatedTime}</p>
+                    <span className="status">{task.state}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section className='completed-tasks'>
+
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
+
+
+/*
+return arr.filter(el => {
+  return el % 2 === 0;
+}).map(el => (<h1>{el}</h1>))
+*/
